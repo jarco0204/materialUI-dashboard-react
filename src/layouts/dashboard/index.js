3ea924c75235bb14e6561bd7dca6e599272bf588
Johan Arcos (El Puma) <jarco0204@outlook.com>
2023-01-24 12:46:33 -0330

    unused vars allowed

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -13,6 +13,8 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+/* eslint-disable no-unused-vars */
+
 // @mui material components
 import Grid from "@mui/material/Grid";
 
